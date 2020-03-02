ca2955cca0f15a74b704c53774f7044ab2562a4e
KurmeD <54103875+KurmeD@users.noreply.github.com>
2020-03-02 13:41:55 +0200

    2veids

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+#from flask import Flask, render_template
+
+#app = Flask(__name__)
 from flask import Flask, render_template
 
-app = Flask(__name__)
+
+app = Flask('app')
 
 @app.route('/')
 def root():
@@ -14,10 +18,20 @@ def spele():
 @app.route('/kalkulators')
 def kalkulators():
   return render_template('kalkulators.html')
+  
+@app.route('/saites')
+def saites():
+  return render_template('saites.html')
+@app.route('/veidotaji')
+def veidotaji():
+  return render_template('veidotaji.html')
 
 @app.route('/rezultati')
 def results():
   return render_template('rezultati.html')
 
 
-app.run(host='0.0.0.0', port=8020)
+#app.run(host='0.0.0.0', port=8020)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
---
+++ static/chats.js
@@ -0,0 +1,79 @@
+const ATJAUNOT = 5000;
+
+async function lasiChatu(){
+    const atbilde = await fetch('/chats/lasi');
+    const datuObjekts = await atbilde.json();
+    //raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
+    await new Promise(resolve=> setTimeout(resolve, ATJAUNOT));
+    await lasiChatu();
+
+}
+
+function  raadiChatuVienkarsi(dati){
+    const jaunaRinda = "</br>";
+    let chats = "";
+    let chataDiv = document.getElementById("chats");
+
+    for(let rinda of dati['chats']){
+        chats = chats + rinda + jaunaRinda;
+    }
+
+    chataDiv.innerHTML = chats;
+
+}
+async function suutiZinju(){
+    let zinjasElements = document.getElementById('zinja');
+    let zinja = zinjasElements.value;
+
+    zinjasElements.value = "";
+
+    const atbilde = await fetch('/chats/suuti', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"chats": zinja})
+    });
+    
+    const datuObjekts = await atbilde.json();
+
+    //raadiChatuVienkarsi(datuObjekts);
+    raadiChataRindas(datuObjekts);
+}
+
+let ievadesLauks = document.getElementById("zinja");
+ievadesLauks.addEventListener("keyup", function(event){
+    if(event.keyCode === 13){
+        suutiZinju();
+    }
+})
+
+
+
+function raadiChataRindas(dati) {
+    const chatUL = document.getElementById("chats");
+    // novaacam ieprieksheejo saturu
+    while (chatUL.firstChild) {
+        chatUL.firstChild.remove();
+    }
+    for (let rinda of dati["chats"]) {
+      chatLI = izveidoJaunuRindu(rinda);
+      chatUL.appendChild(chatLI);
+    }
+    // noskrolleejam uz leju pie peedeejaa chata texta
+    var chatScrollBox = chatUL.parentNode;
+    chatScrollBox.scrollTop = chatScrollBox.scrollHeight;
+  }
+  
+  
+  function izveidoJaunuRindu(zinja) { 
+    let newLI = document.createElement("li");
+    newLI.className = "left clearfix"
+    let newDiv = document.createElement("div"); 
+    newDiv.className = "chat-body clearfix"
+    let newContent = document.createTextNode(zinja); 
+    newLI.appendChild(newDiv); 
+    newDiv.appendChild(newContent); 
+    return newLI;
+  }
